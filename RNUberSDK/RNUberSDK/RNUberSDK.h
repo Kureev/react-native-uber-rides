1ec360b41c23f61f384755ea849092d50d7f298d
Kureev Alexey <kureev-mail@ya.ru>
2016-07-02 01:29:40 +0200

    Replace monolyth framework by static library + framework

---
+++ RNUberSDK/RNUberSDK/RNUberSDK.h
@@ -0,0 +1,19 @@
+//
+//  RNUberSDK.h
+//  RNUberSDK
+//
+//  Created by Alexey Kureev on 30/06/16.
+//  Copyright © 2016 oss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RNUberSDK.
+FOUNDATION_EXPORT double RNUberSDKVersionNumber;
+
+//! Project version string for RNUberSDK.
+FOUNDATION_EXPORT const unsigned char RNUberSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RNUberSDK/PublicHeader.h>
+
+
